396f3b14ab16883111fd71ba9f4d1dcc92bc6b1f
AnasAlgo <mdanas031006@gmail.com>
2026-09-14 19:00:30 +0530

    Added a Integer Program

---
+++ src/J03_Conditionals/C02_CheckInteger.java
@@ -0,0 +1,17 @@
+package J03_Conditionals;
+
+import java.util.Scanner;
+public class C02_CheckInteger {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a real number: ");
+        double number = sc.nextDouble();
+
+        if (number % 1 == 0) {
+            System.out.println((int)number + " is an integer");
+        } else {
+            System.out.println(number + " is a decimal number (not an integer)");
+        }
+    }
+}
