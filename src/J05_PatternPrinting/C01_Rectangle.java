085ae6a67bcd8ecf55cf1bb64c24556fb4c92974
AnasAlgo <mdanas031006@gmail.com>
2026-09-16 13:17:52 +0530

    Added RctanngleProgram

---
+++ src/J05_PatternPrinting/C01_Rectangle.java
@@ -0,0 +1,22 @@
+package J05_PatternPrinting;
+
+import java.util.Scanner;
+
+public class C01_Rectangle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the number of rows: ");
+        int rows = sc.nextInt();
+
+        System.out.print("Enter the number of columns: ");
+        int columns = sc.nextInt();
+
+        for(int i = 1; i<=rows; i++){
+            for(int j = 1; j<=columns; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
