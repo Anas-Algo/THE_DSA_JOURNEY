b9aab72fd3b59301d09a84952aea78fd0a137844
AnasAlgo <mdanas031006@gmail.com>
2026-09-23 16:20:41 +0530

    Added NumberTriangle Problem

---
+++ src/J05_PatternPrinting/C08_NumberTriangle.java
@@ -0,0 +1,27 @@
+package J05_PatternPrinting;
+
+import java.util.Scanner;
+
+public class C08_NumberTriangle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter num: ");
+        int n = sc.nextInt();
+
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= i; j++){
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+    }
+}
+/*
+    Logic:
+    I used nested loops where the outer loop controls the number of rows
+    and the inner loop prints the numbers in each row.
+    The inner loop starts from 1 and runs up to the current row number (i),
+    so each new row contains one more number than the previous row.
+    This creates an increasing number triangle pattern.
+*/
